4f9e1b37b858cf79513eb5fb83b7d6356fd42ffd
naelec <naelec@gmail.com>
2025-04-11 18:56:32 +0700

    Add SW to toggle LED1 and Blink LED2

--- main.c
+++ main.c
@@ -9,11 +9,19 @@ void main (void)
 {
     WDTCTL = WDTPW | WDTHOLD; //Hold Watch dog
     P1DIR = 0x01; //PIN1.0 is output
+    P4DIR = 0b10000000; //PIN4.7 is output
+    P1REN  = 0b00000010; //Pull up resistor at P1.1
+    P1OUT |= 0b00000010; //Pull up resistor at P1.1
    
-    while (1) {
-    P1OUT |= 0x01;  // P1.0 High
-    __delay_cycles(100000);
-    P1OUT &= 0xFE; //P1.0 Low
-    __delay_cycles(100000);
-    }
+   while (1) {
+        P4OUT |= 0b10000000; //Output P4.7 (LED2) Off LED (active low)        
+        if (!(P1IN & BIT1)) {   // If P1.1 is LOW (switch pressed)
+            __delay_cycles(10000);  //Debouce delay
+            while (!(P1IN & BIT1)); //Wait switch release
+            P1OUT ^= BIT0;     // 
+        }
+        __delay_cycles(100000);
+        P4OUT = 0x00;    //On LED
+        __delay_cycles(100000);
+   }      
 }
